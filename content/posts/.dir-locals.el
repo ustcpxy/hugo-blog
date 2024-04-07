c77c224eb6466ac209462d8c39e7bca48597bc97
Derek Pan <ustcpxy@gmail.com>
2024-04-07 19:10:59 +0800

    support blog posts as silo denote

---
+++ content/posts/.dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables.  For more information evaluate:
+;;;
+;;;     (info "(emacs) Directory Variables")
+
+((nil . ((denote-directory . default-directory)
+         (denote-file-type . "markdown-yaml")
+         (denote-known-keywords . ("emacs" "linux"))
+	 	 (denote-infer-keywords . nil)
+	 )))
